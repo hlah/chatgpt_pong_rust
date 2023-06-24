e0348c2eb8cdcbc574421bc5c8320ce14da1c497
Mateus Davi Simon <mateusdsimon@gmail.com>
2023-06-24 17:51:48 -0300

    Segunda alternativa, tentativa 2, iteração 1.

--- var_2_try_2/README.md
+++ var_2_try_2/README.md
@@ -1,7 +1,7 @@
 
 Experimento número 2 usando prompt inicial: "Crie um clone de Pong usando a linguagem de programação Rust com o biblioteca kiss3d versão 0.26".
 
-Chat: https://chat.openai.com/share/5df9b40b-b3ec-42a6-883c-eac81dd8376a
-
 ## Resultados
+
+    - Iteração 1: erro de compilação, não foi declarado duas dependências utilizadas e uso de método inexistente.
 
--- var_2_try_2/src/main.rs
+++ var_2_try_2/src/main.rs
@@ -1,3 +1,96 @@
+extern crate kiss3d;
+extern crate nalgebra as na;
+extern crate rand;
+
+use kiss3d::event::{Action, Key, WindowEvent};
+use kiss3d::light::Light;
+use kiss3d::scene::SceneNode;
+use kiss3d::window::Window;
+use na::{Point3, Vector2};
+
+const PADDLE_WIDTH: f32 = 0.2;
+const PADDLE_HEIGHT: f32 = 1.0;
+const BALL_RADIUS: f32 = 0.1;
+
+struct Paddle {
+    node: SceneNode,
+    position: Vector2<f32>,
+    velocity: Vector2<f32>,
+}
+
+impl Paddle {
+    fn new(x: f32, y: f32) -> Paddle {
+        let mut node = Window::new("Pong").add_cube(PADDLE_WIDTH, PADDLE_HEIGHT, 0.1);
+        node.set_color(0.0, 1.0, 0.0);
+
+        Paddle {
+            node,
+            position: Vector2::new(x, y),
+            velocity: Vector2::new(0.0, 0.0),
+        }
+    }
+
+    fn update(&mut self, dt: f32) {
+        self.position += self.velocity * dt;
+        self.node
+            .set_local_translation(Point3::new(self.position.x, self.position.y, 0.0));
+    }
+}
+
+struct Ball {
+    node: SceneNode,
+    position: Vector2<f32>,
+    velocity: Vector2<f32>,
+}
+
+impl Ball {
+    fn new(x: f32, y: f32) -> Ball {
+        let mut node = Window::new("Pong").add_sphere(BALL_RADIUS);
+        node.set_color(1.0, 0.0, 0.0);
+
+        Ball {
+            node,
+            position: Vector2::new(x, y),
+            velocity: Vector2::new(0.1, 0.1),
+        }
+    }
+
+    fn update(&mut self, dt: f32) {
+        self.position += self.velocity * dt;
+        self.node
+            .set_local_translation(Point3::new(self.position.x, self.position.y, 0.0));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new("Pong");
+    window.set_light(Light::StickToCamera);
+
+    let mut left_paddle = Paddle::new(-6.0, 0.0);
+    let mut right_paddle = Paddle::new(6.0, 0.0);
+    let mut ball = Ball::new(0.0, 0.0);
+
+    while window.render() {
+        let dt = window.scene_mut().time().delta_seconds();
+
+        if window.get_key(Key::W) == Action::Press {
+            left_paddle.velocity.y = 1.0;
+        } else if window.get_key(Key::S) == Action::Press {
+            left_paddle.velocity.y = -1.0;
+        } else {
+            left_paddle.velocity.y = 0.0;
+        }
+
+        if window.get_key(Key::Up) == Action::Press {
+            right_paddle.velocity.y = 1.0;
+        } else if window.get_key(Key::Down) == Action::Press {
+            right_paddle.velocity.y = -1.0;
+        } else {
+            right_paddle.velocity.y = 0.0;
+        }
+
+        left_paddle.update(dt);
+        right_paddle.update(dt);
+        ball.update(dt);
+    }
 }
